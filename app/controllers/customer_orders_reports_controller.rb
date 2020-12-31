4cbba20cd5f099e76104abd51dc9eb33dec1559b
sandeepparhadonror <sandeepparhadonror@gmail.com>
2020-12-31 02:33:49 -0800

    Update Gemfile and ransack uniq record

--- app/controllers/customer_orders_reports_controller.rb
+++ app/controllers/customer_orders_reports_controller.rb
@@ -7,7 +7,7 @@ end
 
 def index
   @q = Customer.ransack(params[:q])
-  @customer_orders = @q.result.includes(:orders)
+  @customer_orders = @q.result.includes(:orders).to_a.uniq
   #@q.build_condition if @q.conditions.empty?
   #@q.build_sort if @q.sorts.empty?
 end
